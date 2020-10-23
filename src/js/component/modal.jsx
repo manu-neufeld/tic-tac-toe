f18caec690e6e903e80faba5359735e8c1907a59
manu-neufeld <manuela.neufeld@gmail.com>
2020-10-23 11:00:12 +0000

    table added

--- src/js/component/modal.jsx
+++ src/js/component/modal.jsx
@@ -5,6 +5,7 @@ import { useEffect } from "react";
 export const Modal = props => {
 	const [opened, setOpened] = useState(true);
 	const [player, setPlayer] = useState(1);
+	let posiblePositions = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
 	return opened ? (
 		<div>
 			<h2>Choose your weapon</h2>
@@ -26,8 +27,23 @@ export const Modal = props => {
 			</button>
 		</div>
 	) : (
-		<button type="button" onClick={() => window.location.reload(false)}>
-			Restart
-		</button>
+		<div>
+			<button
+				type="button"
+				onClick={() => {
+					window.location.reload(false);
+				}}>
+				Restart
+			</button>
+			<div className="row">
+				{posiblePositions.map((item, index) => (
+					<div key={index} className="border col-4">
+						{item}
+					</div>
+				))}
+			</div>
+		</div>
 	);
 };
+
+Modal.PropTypes = {};
